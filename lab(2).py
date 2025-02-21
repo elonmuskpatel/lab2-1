58b34566b683cc1a843a9b99e27ec369205e398f
elonmuskpatel <devam201877@gmail.com>
2025-02-21 10:10:16 +0530

    Add files via upload

---
+++ lab(2).py
@@ -0,0 +1,9 @@
+a=int(input("enter value of NO1:"))
+b=int(input("enter value of NO2:"))
+c=int(input("enter value of NO3:"))
+if(a>b and b>c):
+    print(a,"is the largest")
+elif(b>a and b>c):
+    print(b,"is the largset")
+else:
+    print(c,"is the largest")
---
+++ lab2(1).py
@@ -0,0 +1,6 @@
+x=12
+y=20
+if x>=y :
+    print("x is largest value")
+else:
+        print("x is smallest value")
